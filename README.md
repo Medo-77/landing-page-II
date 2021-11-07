52d23807cc1c32c80f0a5221b1f537ff58ae89e8
mohamed elsaid <91749427+Medo-77@users.noreply.github.com>
2021-11-07 13:23:02 +0200

    new project landing page

---
+++ README.md
@@ -0,0 +1,8 @@
+# Landing Page Project
+
+
+## definition 
+
+it is landing page which has a nav-bar to scroll into it`s sections 
+*in this project we turn our understanding of JS and DOM into real work
+* this project dynamically create the nav-bar and scroll into the target section by using JS
---
+++ js/app.js
@@ -0,0 +1,116 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+
+const list= document.getElementById('navbar__list');
+const parts= document.querySelectorAll('section');
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
+function menuItem() {
+    "use strict";
+    let sec;
+    for( sec of parts)
+    {
+        let secName=sec.getAttribute("data-nav");
+        let secPlace=sec.getAttribute("id");
+        //create the sections of nav-bar
+       let items=document.createElement('li');
+
+        items.innerHTML =`<a href='#${secPlace}' > ${secName} </a>`;
+        //added the content of section
+
+
+        items.addEventListener('scroll',
+        (event)=>{event.preventDefault();
+          sec.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})});
+
+
+        list.appendChild(items);
+        //added section to the nav-bar
+}
+};
+
+function calling (intity){
+  "use strict";
+  let int;
+
+  for (int  of intity)
+
+  {  if (int.isIntersecting)
+    {
+     parts.forEach((item) => {
+    if
+     (item.classList.contains('your-active-class'))
+     {item.classList.remove('your-active-class')}
+
+  });
+
+      int.target.classList.add("your-active-class");
+let medo =int.target.getAttribute("data-nav");
+}
+}};
+
+
+let  properties = {
+  root: document.querySelector('#scrollArea'),
+  rootMargin: '0px',
+  threshold: 0.45
+};
+
+let view = new IntersectionObserver(calling, properties);
+for (part of parts){view.observe(part)};
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+menuItem();
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
