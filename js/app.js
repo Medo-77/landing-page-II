986c95efc1c58f1f7cedc1f21cdc230e3b4f5b7e
mohamed elsaid <91749427+Medo-77@users.noreply.github.com>
2021-12-24 21:31:42 +0200

    Highlighting

--- js/app.js
+++ js/app.js
@@ -1,38 +1,11 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
 const list = document.getElementById('navbar__list');
 const parts = document.querySelectorAll('section');
 
 
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+
 function menuItem() {
     "use strict";
     
@@ -42,25 +15,47 @@ function menuItem() {
         //create the sections of nav-bar
         const items = document.createElement('li');
         items.innerHTML = `<a href='#${secPlace}' > ${secName} </a>`;
-            
+       
                 items.addEventListener('click',
         (event) => {
             event.preventDefault();
-            sec.scrollIntoView({
-                behavior: "smooth",
-                block: "end",
-                inline: "nearest"
-            })
-        })
-        
+                    
+                     if(items.classList.contains('active'))
+
+                     
+                     {items.classList.remove('active')}                  
+                     
+                     items.classList.add('active')
+                     
+
+
+                     sec.scrollIntoView({
+                        behavior: "smooth",
+                        block: "end",
+                        inline: "nearest"
+                    })
+                    })
+
+                
+            
+            
+            
+          
     
-    ;
+        
     
         //added the content of section
         list.appendChild(items);
-        //added section to the nav-bar
+        }
     }
-};
+   
+        //added section to the nav-bar
+       
+    
+;
+
+
+
 
 function calling(intity) {
     "use strict";
@@ -95,12 +90,8 @@ let view = new IntersectionObserver(calling, properties);
 for (part of parts) {
     view.observe(part)
 };
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-// it is the code to create button of scrolling
+
+
 
 
 
@@ -112,23 +103,3 @@ menuItem();
 
 
 
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
