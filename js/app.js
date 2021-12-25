070fef9418f70504f901015cb62f2100c2332684
mohamed elsaid <91749427+Medo-77@users.noreply.github.com>
2021-12-25 14:30:50 +0200

    last edit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const list = document.getElementById('navbar__list');
 const parts = document.querySelectorAll('section');
 
@@ -6,53 +5,41 @@ const parts = document.querySelectorAll('section');
 
 
 
+
+
 function menuItem() {
     "use strict";
-    
-    for (let sec of parts) {
+    for (let sec of parts) 
+    {
         let secName = sec.getAttribute("data-nav");
         const secPlace = sec.getAttribute("id");
         //create the sections of nav-bar
         const items = document.createElement('li');
+        items.className = "navBarItem";
         items.innerHTML = `<a href='#${secPlace}' > ${secName} </a>`;
-       
-                items.addEventListener('click',
-        (event) => {
-            event.preventDefault();
-                    
-                     if(items.classList.contains('active'))
-
-                     
-                     {items.classList.remove('active')}                  
-                     
-                     items.classList.add('active')
-                     
-
-
-                     sec.scrollIntoView({
-                        behavior: "smooth",
-                        block: "end",
-                        inline: "nearest"
-                    })
-                    })
-
-                
-            
-            
-            
-          
-    
-        
-    
+        items.addEventListener('click',
+            (event) => {
+                event.preventDefault();
+                let navBarItems = document.getElementsByClassName('navBarItem');
+                Array.prototype.forEach.call(navBarItems, (item) => {
+                    if (item == event.path[1]) {
+                        if (!event.path[1].classList.contains('active'))
+                            event.path[1].classList.add('active');
+                    } else {
+                        if (item.classList.contains('active'))
+                            item.classList.remove('active');
+                    }
+                });
+                sec.scrollIntoView({
+                    behavior: "smooth",
+                    block: "end",
+                    inline: "nearest"
+                })
+            });
         //added the content of section
         list.appendChild(items);
-        }
     }
-   
-        //added section to the nav-bar
-       
-    
-;
+}
 
 
 
@@ -94,12 +81,4 @@ for (part of parts) {
 
 
 
-
-
-
 menuItem();
-
-
-
-
-
